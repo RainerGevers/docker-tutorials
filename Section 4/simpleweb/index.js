dc1bfb18224b95911f327dd08ce0d29f04ec4643
Rainer Gevers <rainer@fastcomm.com>
2020-01-24 11:44:52 +0200

    updated code

--- Section 4/simpleweb/index.js
+++ Section 4/simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send('How are you doing');
+  res.send('Bye there');
 });
  
 app.listen(8080, () => {
