20b9b95e6c729fb68a5cdf2057dda4a57f60a1cc
Rainer Gevers <rainer@fastcomm.com>
2020-02-06 10:59:56 +0200

    updated docker and compose files

--- Section 8/complex/client/src/Fib.js
+++ Section 8/complex/client/src/Fib.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class Fib extents Component {
+class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
